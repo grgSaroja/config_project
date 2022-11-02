1b169d13c27240bbad724f99d8f4502a5902422c
Halooween <saroja.grg33@gmail.com>
2022-11-02 11:08:49 +0545

    first insertion

---
+++ app/Http/Controllers/settleController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
+
+class settleController extends Controller
+{
+    public function index(){
+       return "maintainance mode";
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\settleController;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+
+  //  dd(config('app.timezone'));
+     //dd(env('APP_NAME'));
+
+  $environment = App::environment();
+
+
+    return view('welcome', compact('environment'));
+});
+
+Route::get('/setup', [settleController::class, 'index'])->name('setup');
